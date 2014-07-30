843711f6f6cc77b5ed95152f07ffabd521924c49
eyozhensin <zhensin91@gmail.com>
2014-07-30 16:04:17 -0700

    Account model and related files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :accounts
+
   get 'dashboard/index'
 root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
